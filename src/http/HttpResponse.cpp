796acf9216730800b1feb3d9b4b6ce68b1fd4b8e
Dardo95 <dsanto-tomas@outlook.com>
2026-06-25 13:09:47 +0200

    feat: add HTTP response builder

---
+++ src/http/HttpResponse.cpp
@@ -0,0 +1,36 @@
+#include "http/HttpResponse.hpp"
+
+std::string buildResponse(int statusCode, const std::string& contentType, const std::string& body) {
+    static std::map<int, std::string> statusTexts;
+    if (statusTexts.empty()) {
+        statusTexts[200] = "OK";
+        statusTexts[201] = "Created";
+        statusTexts[404] = "Not Found";
+        statusTexts[403] = "Forbidden";
+        statusTexts[405] = "Method Not Allowed";
+        statusTexts[500] = "Internal Server Error";
+    }
+
+    std::string statusText = "Unknown";
+    std::map<int, std::string>::iterator it = statusTexts.find(statusCode);
+    if (it != statusTexts.end()) {
+        statusText = it->second;
+    }
+
+    std::ostringstream oss;
+    oss << statusCode;
+    std::string codeStr = oss.str();
+
+    std::ostringstream lenOss;
+    lenOss << body.length();
+    std::string lengthStr = lenOss.str();
+
+    std::string response;
+    response += "HTTP/1.1 " + codeStr + " " + statusText + "\r\n";
+    response += "Content-Type: " + contentType + "\r\n";
+    response += "Content-Length: " + lengthStr + "\r\n";
+    response += "\r\n";
+    response += body;
+
+    return response;
+}
